6e6b4d2c84abf15a6a88b595702e255302dc7a50
Maikhaa <maikha.abarca@gmail.com>
2021-04-22 20:27:09 +0100

    weather_info_test refactored

--- test/widget/weather_info_test.dart
+++ test/widget/weather_info_test.dart
@@ -1,25 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_simple_demo/bloc/weather_bloc.dart';
+import 'package:flutter_bloc_simple_demo/data/weather_repo.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_bloc_simple_demo/main.dart';
 import 'package:flutter_bloc_simple_demo/widgets/weather_info.dart';
 
+import '../mock_helpers/mock_client_helper.dart';
+
 void main() {
+  final mockHelper = MockClientHelper();
+  final mockClient = mockHelper.getMockClient;
+
+  Widget setUpWidget(Widget widget) {
+    return BlocProvider(
+      create: (BuildContext context) => WeatherBloc(WeatherRepo(mockClient)),
+      child: MaterialApp(
+        home: widget,
+      ),
+    );
+  }
+
   testWidgets('Finds WeatherInfo', (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(home: Home()));
+    await tester.pumpWidget(setUpWidget(Home()));
     var weatherInfo = find.byType(WeatherInfo);
     expect(weatherInfo, findsOneWidget);
   });
 
   testWidgets('WeatherInfo changes to loaded state', (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(home: Home()));
+    await tester.pumpWidget(setUpWidget(Home()));
 
     var textField = find.byKey(Key('search-input'));
     var searchButton = find.byKey(Key('search-button'));
 
-    await tester.enterText(textField, "London");
+    await tester.enterText(textField, "london");
     await tester.tap(searchButton);
     await tester.pump();
 
-    expect(find.byKey(Key('weather-info')), findsOneWidget);
+    expectLater(find.byKey(Key('weather-info')), findsOneWidget);
   });
 }
