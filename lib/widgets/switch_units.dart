71d997f57f874249c5fea4c145363f53b97c9565
Maikhaa <maikha.abarca@gmail.com>
2021-04-25 16:03:08 +0100

    Unused import deleted

--- lib/widgets/switch_units.dart
+++ lib/widgets/switch_units.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_simple_demo/bloc/weather_bloc.dart';
-import 'package:flutter_bloc_simple_demo/constants.dart';
 
 class SwitchUnits extends StatelessWidget {
   @override
